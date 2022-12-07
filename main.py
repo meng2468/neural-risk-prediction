7a26896b44a158c7703de44dd6c0384ab74954f7
meng2468 <mandm116@gmail.com>
2022-12-08 00:43:40 +0800

    Add training and testing run script

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import torch
+from torch import nn
+
 from dataloader import EICUDataSet
 from torch.utils.data import Dataset, DataLoader
+from models import BaseRecurrent
+
+from optimization import train_model, test_loop
 
 if __name__ == '__main__':
-    data = EICUDataSet('eicu_prepared_x.csv','eicu_prepared_y.csv')
-    loader = DataLoader(data, batch_size=30, shuffle=True)
+    learning_rate = 1e-2
+    batch_size = 64
+    epochs = 7
 
-    print(data[0])
+    loss_fn =  nn.CrossEntropyLoss()
+
+    data = EICUDataSet('eicu_prepared_x.csv','eicu_prepared_y.csv')
+    loader = DataLoader(data, batch_size=batch_size, shuffle=True)
+
+    model = BaseRecurrent()
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    for epoch in range(epochs):
+        train_model(loader, model, loss_fn, optimizer)
+        test_loop(loader, model, loss_fn)
 
