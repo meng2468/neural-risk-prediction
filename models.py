bdac809a0cfe8fe14ac9fe1d7b6b98c90e7eb405
meng2468 <mandm116@gmail.com>
2022-12-08 00:42:35 +0800

    Reformat output tensors for rnn forward passing

--- models.py
+++ models.py
@@ -11,9 +11,9 @@ class BaseRecurrent(nn.Module):
         self.flatten = nn.Flatten()
         self.recurrent = nn.RNN(input_size=33, hidden_size=30)
         self.relu = nn.ReLU()
-        self.final = nn.Linear(in_features=30,out_features=2)
+        self.final = nn.Linear(in_features=450,out_features=1)
 
     def forward(self, x):
-        hidden_state = self.recurrent(x)
-        logits = self.relu(self.final(hidden_state[-1]))
+        out, hidden = self.recurrent(x)
+        logits = self.relu(self.final(out.view(-1, 450)))
         return logits
