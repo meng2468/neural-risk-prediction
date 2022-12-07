886e62a334dbe4fbe52ced6adcd494e009792eb8
meng2468 <mandm116@gmail.com>
2022-12-08 00:42:59 +0800

    Add train and test loops

---
+++ optimization.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+
+
+
+def train_model(dataloader, model, loss_fn, optimizer):
+    size=len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        pred = model(X)
+        loss = loss_fn(pred.view(-1), y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 40 == 0:
+            # print(pred, y), loss
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred.view(-1), y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
