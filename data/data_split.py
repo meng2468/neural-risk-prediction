b00d8637b36c80bfe89b753e739833cb1c45899d
meng2468 <mandm116@gmail.com>
2022-12-09 11:25:56 +0800

    Add code to split transformed data to train, test, val

---
+++ data/data_split.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+data_x = pd.read_csv('data/eicu_prepared_x.csv',index_col='id')
+labels = pd.read_csv('data/eicu_prepared_y.csv', index_col='id')
+
+train, test = train_test_split(data_x.index.unique(), train_size=.75, stratify=labels.survival_90days)
+test, val = train_test_split(test, train_size=.6, stratify=labels.loc[test].survival_90days)
+
+def reset_ids(df):
+    patient_to_id = {}
+    patients = df['patientunitstayid'].drop_duplicates()
+    i = 0
+    for x in patients:
+        patient_to_id[x] = i
+        i += 1
+    df = df.reset_index()
+    df['id'] = df['patientunitstayid'].apply(lambda x: patient_to_id[x])
+    df = df.set_index('id')
+    return df
+
+reset_ids(data_x.loc[train]).to_csv('data/eicu_train_x.csv')
+reset_ids(data_x.loc[test]).to_csv('data/eicu_test_x.csv')
+reset_ids(data_x.loc[val]).to_csv('data/eicu_val_x.csv')
+
+reset_ids(labels.loc[train]).to_csv('data/eicu_train_y.csv')
+reset_ids(labels.loc[test]).to_csv('data/eicu_test_y.csv')
+reset_ids(labels.loc[val]).to_csv('data/eicu_val_y.csv')
