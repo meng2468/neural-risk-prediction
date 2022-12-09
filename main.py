00fd9f5146db896b2c8c3036592cb359fb40c560
meng2468 <mandm116@gmail.com>
2022-12-09 11:27:19 +0800

    Add data loader for each data split

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ from optimization import train_model, test_loop
 if __name__ == '__main__':
     learning_rate = 1e-2
     batch_size = 64
-    epochs = 7
+    epochs = 6
 
     loss_fn =  nn.CrossEntropyLoss()
 
-    data = EICUDataSet('eicu_prepared_x.csv','eicu_prepared_y.csv')
-    loader = DataLoader(data, batch_size=batch_size, shuffle=True)
+    train_data = EICUDataSet('data/eicu_train_x.csv','data/eicu_train_y.csv')
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+
+    test_data = EICUDataSet('data/eicu_test_x.csv','data/eicu_test_y.csv')
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+
+    val_data = EICUDataSet('data/eicu_val_x.csv','data/eicu_val_y.csv')
+    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
 
     model = BaseRecurrent()
 
@@ -24,7 +30,9 @@ if __name__ == '__main__':
     for epoch in range(epochs):
         print('*'*20)
         print('Running Epoch', epoch)
-        # test_loop(loader, model, loss_fn)
-        train_model(loader, model, loss_fn, optimizer)
+        
+        train_model(train_loader, model, loss_fn, optimizer)
+        test_loop(val_loader, model, loss_fn)
+        test_loop(test_loader, model, loss_fn)
 
     torch.save(model.state_dict, 'base_model.ts')
