671ced4e10be496eb00ef27189d0342e50d9ba4f
meng2468 <mandm116@gmail.com>
2022-12-09 19:18:55 +0800

    Add layered RNN

--- models.py
+++ models.py
@@ -16,3 +16,15 @@ class BaseRecurrent(nn.Module):
         out, hidden = self.recurrent(x)
         logits = (self.final(out[:,-1,:]))
         return logits
+
+class LayeredRecurrent(nn.Module):
+    def __init__(self):
+        super(LayeredRecurrent, self).__init__()
+        self.hidden_size = 120
+        self.recurrent = nn.RNN(input_size=33, hidden_size=self.hidden_size, num_layers=3, dropout=.5)
+        self.final = nn.Linear(in_features=self.hidden_size,out_features=2)
+
+    def forward(self, x):
+        out, hidden = self.recurrent(x)
+        logits = (self.final(out[:,-1,:]))
+        return logits
