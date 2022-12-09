9eeffbe811a1bc062dc2fde16ab50ccabdcdc184
meng2468 <mandm116@gmail.com>
2022-12-09 12:09:49 +0800

    Add loss tracking for training

--- optimization.py
+++ optimization.py
@@ -1,14 +1,17 @@
 import torch
 from torch import nn
 
-
-
 def train_model(dataloader, model, loss_fn, optimizer):
     size=len(dataloader.dataset)
+    total_loss = 0
+    correct = 0
+
     for batch, (X, y) in enumerate(dataloader):
         pred = model(X)
 
         loss = loss_fn(pred.view(-1, 2), y)
+        total_loss += loss
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
         # Backpropagation
         optimizer.zero_grad()
@@ -20,6 +23,11 @@ def train_model(dataloader, model, loss_fn, optimizer):
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
+    total_loss /= len(dataloader)
+    correct /= size
+    print(f"Train  Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {total_loss:>8f} \n")
+    return total_loss, correct
+
 def test_loop(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
@@ -28,9 +36,11 @@ def test_loop(dataloader, model, loss_fn):
     with torch.no_grad():
         for X, y in dataloader:
             pred = model(X)
-            test_loss += loss_fn(pred.view(-1), y).item()
+            test_loss += loss_fn(pred.view(-1, 2), y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    return test_loss, correct
