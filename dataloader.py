92a8f2d80e3f8bf02a678f030d7f16819207690f
meng2468 <mandm116@gmail.com>
2022-12-11 22:39:19 +0800

    Add MIMIC dataloader

--- dataloader.py
+++ dataloader.py
@@ -24,3 +24,23 @@ class EICUDataSet(Dataset):
         else:
             y = 1
         return x, torch.tensor(np.array(y, dtype='f'))
+
+class MIMICDataSet(Dataset):
+    def __init__(self, csv_file_x, csv_file_y):
+        self.data_x = pd.read_csv(csv_file_x, index_col='id')
+        self.labels = pd.read_csv(csv_file_y, index_col='id')
+        self.times = self.data_x.t.unique()
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        x = torch.tensor(np.array([self.data_x.loc[idx].query('t == '+str(x)).value.values for x in self.times], dtype='f'))
+        if self.labels.loc[idx]['90_days_survival'] == 'alive':
+            y = 0
+        else:
+            y = 1
+        return x, torch.tensor(np.array(y, dtype='f'))
