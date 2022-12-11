9b98579269f1a6a62a4fa1f4cf89eef7143a2250
meng2468 <mandm116@gmail.com>
2022-12-11 19:39:13 +0800

    Increase hidden size to 300

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ print(f"Using {device} device")
 class BaseRecurrent(nn.Module):
     def __init__(self):
         super(BaseRecurrent, self).__init__()
-        self.hidden_size = 120
+        self.hidden_size = 300
         self.recurrent = nn.RNN(input_size=33, hidden_size=self.hidden_size)
         self.final = nn.Linear(in_features=self.hidden_size,out_features=1)
         self.sigmoid = nn.Sigmoid()
@@ -22,7 +22,7 @@ class BaseRecurrent(nn.Module):
 class LayeredRecurrent(nn.Module):
     def __init__(self):
         super(LayeredRecurrent, self).__init__()
-        self.hidden_size = 120
+        self.hidden_size = 300
         self.recurrent = nn.RNN(input_size=33, hidden_size=self.hidden_size, num_layers=3, dropout=.2)
         self.final = nn.Linear(in_features=self.hidden_size,out_features=1)
         self.sigmoid = nn.Sigmoid()
@@ -37,7 +37,7 @@ class LayeredRecurrent(nn.Module):
 class BaseLSTM(nn.Module):
     def __init__(self):
         super(BaseLSTM, self).__init__()
-        self.hidden_size=120
+        self.hidden_size=300
         self.lstm = nn.LSTM(input_size=33, hidden_size=self.hidden_size)
         self.final = nn.Linear(in_features=self.hidden_size, out_features=1)
         self.sigmoid = nn.Sigmoid()
@@ -51,7 +51,7 @@ class BaseLSTM(nn.Module):
 class BaseGRU(nn.Module):
     def __init__(self):
         super(BaseGRU, self).__init__()
-        self.hidden_size=120
+        self.hidden_size=300
         self.lstm = nn.GRU(input_size=33, hidden_size=self.hidden_size)
         self.final = nn.Linear(in_features=self.hidden_size, out_features=1)
         self.sigmoid = nn.Sigmoid()
