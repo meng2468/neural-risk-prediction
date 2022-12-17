2f4692eabd337335d744afc7fa5794d806efa896
meng2468 <mandm116@gmail.com>
2022-12-17 16:39:44 +0800

    Update input size for interpolated data

--- mimic_models.py
+++ mimic_models.py
@@ -9,7 +9,7 @@ class BaseRecurrent(nn.Module):
     def __init__(self):
         super(BaseRecurrent, self).__init__()
         self.hidden_size = 300
-        self.recurrent = nn.RNN(input_size=53, hidden_size=self.hidden_size)
+        self.recurrent = nn.RNN(input_size=51, hidden_size=self.hidden_size)
         self.final = nn.Linear(in_features=self.hidden_size,out_features=1)
         self.sigmoid = nn.Sigmoid()
 
@@ -23,7 +23,7 @@ class LayeredRecurrent(nn.Module):
     def __init__(self):
         super(LayeredRecurrent, self).__init__()
         self.hidden_size = 300
-        self.recurrent = nn.RNN(input_size=53, hidden_size=self.hidden_size, num_layers=3, dropout=.2)
+        self.recurrent = nn.RNN(input_size=51, hidden_size=self.hidden_size, num_layers=3, dropout=.2)
         self.final = nn.Linear(in_features=self.hidden_size,out_features=1)
         self.sigmoid = nn.Sigmoid()
 
@@ -38,7 +38,7 @@ class BaseLSTM(nn.Module):
     def __init__(self):
         super(BaseLSTM, self).__init__()
         self.hidden_size=300
-        self.lstm = nn.LSTM(input_size=53, hidden_size=self.hidden_size)
+        self.lstm = nn.LSTM(input_size=51, hidden_size=self.hidden_size)
         self.final = nn.Linear(in_features=self.hidden_size, out_features=1)
         self.sigmoid = nn.Sigmoid()
 
@@ -52,7 +52,7 @@ class BaseGRU(nn.Module):
     def __init__(self):
         super(BaseGRU, self).__init__()
         self.hidden_size=300
-        self.lstm = nn.GRU(input_size=53, hidden_size=self.hidden_size)
+        self.lstm = nn.GRU(input_size=51, hidden_size=self.hidden_size)
         self.final = nn.Linear(in_features=self.hidden_size, out_features=1)
         self.sigmoid = nn.Sigmoid()
 
