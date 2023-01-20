41c078fa332a4a772f35736067f5f7f768c9abe6
meng2468 <mandm116@gmail.com>
2023-01-20 12:49:42 +0800

    Add demographic data to output cvs

--- eicu_loading.py
+++ eicu_loading.py
@@ -1,106 +1,63 @@
-#!conda activate thesis
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import os
 
-file_name = os.path.join('../Data/eicu',os.listdir('../Data/eicu')[0])
-
-print('Pulling data', file_name)
-# In[3]:
-
-
-df = pd.read_excel(file_name)
-df = df.query('patientunitstayid != 708912')
-
-
-
-# In[120]:
-
-
+from sklearn import preprocessing
+import numpy as np
 import re
 
-ts_features = set(re.findall('(\w+\_\d+)\s',' '.join(df.columns)+' '))
-measurements = set([re.findall('(\w+\_*\w*)\_\d+', x)[0] for x in ts_features])
-cat_features = list(df.drop(list(ts_features), axis=1).columns.values) + ['sxyl_lactate_1', 'sxyl_lactate_2', 'labresultoffset_1', 'labresultoffset_2']
 
+if __name__ == '__main__':
+    file_name = os.path.join('../Data/eicu',os.listdir('../Data/eicu')[0])
 
-# In[123]:
+    print('Pulling data from', file_name)
+    df = pd.read_excel(file_name)
+    df = df.query('patientunitstayid != 708912')
 
+    ts_features = set(re.findall('(\w+\_\d+)\s',' '.join(df.columns)+' '))
+    measurements = set([re.findall('(\w+\_*\w*)\_\d+', x)[0] for x in ts_features])
+    cat_features = list(df.drop(list(ts_features), axis=1).columns.values) + ['sxyl_lactate_1', 'sxyl_lactate_2', 'labresultoffset_1', 'labresultoffset_2']
 
-df_cat = df[cat_features]
+    df_cat = df[cat_features]
 
+    df_singles = {}
+    for x in measurements:
+        df_x = df[['patientunitstayid'] + [y for y in ts_features if y.startswith(x)]]
+        df_singles[x] = df_x
 
-# In[159]:
-print('Splitting out individual measurements')
+    df_msingles = {}
+    for k,v in df_singles.items():
+        v = pd.melt(v, id_vars=[v.columns.values[0]], value_vars=list(v.columns.values))
+        v['t'] = v.variable.apply(lambda x: re.findall('\_(\d+)', x)[0]).astype(int) 
+        if v['t'].nunique() == 15:
+            v['variable'] = k
+            df_msingles[k] = v
+        else:
+            print('Not time series but grouped here', k)
 
-df_singles = {}
-for x in measurements:
-    df_x = df[['patientunitstayid'] + [y for y in ts_features if y.startswith(x)]]
-    df_singles[x] = df_x
+    df_t_pheno =  pd.concat([v for k, v in df_msingles.items()], ignore_index=True)
 
+    intervention = ['treatment',
+        'treatmentoffset', 'mbp', 'mbp_offset', 'crystalloids',
+        'crystalloidsoffset', 'vasoactive_drugs', 'vasoactive_drugs_offset',
+        'invasive_line', 'invasive_lineoffset', 'hydrocortisone',
+        'hydrocortisoneoffset', 'nursecare', 'nursecareoffset', 'dialysis',
+        'dialysisoffset', 'survival_90days', 'sxyl_lactate_1', 'sxyl_lactate_2',
+        'labresultoffset_1', 'labresultoffset_2']
 
-# In[160]:
-print('Renaming columns and ts')
+    ts_features = list(set(re.findall('(\w+\_\d+)\s',' '.join(df.columns)+' ')))
+    df_cat = df.drop(columns=list(ts_features)+intervention+['microlab','chartime'])
 
-df_msingles = {}
-for k,v in df_singles.items():
-    v = pd.melt(v, id_vars=[v.columns.values[0]], value_vars=list(v.columns.values))
-    v['t'] = v.variable.apply(lambda x: re.findall('\_(\d+)', x)[0]).astype(int) 
-    if v['t'].nunique() == 15:
-        v['variable'] = k
-        df_msingles[k] = v
-    else:
-        print('Not time series but grouped here', k)
+    enc = preprocessing.OrdinalEncoder()
+    enc.fit(df_cat[['gender','ethnicity','age']])
+    df_cat[['gender','ethnicity','age']] = enc.transform(df_cat[['gender','ethnicity','age']])
 
+    times = df_t_pheno.t.unique()
+    df_cat_pivot = pd.melt(df_cat, id_vars='patientunitstayid')
+    df_var_t = pd.melt(pd.DataFrame({x: times for x in df_cat_pivot.variable.unique()})).rename(columns={'value':'t'})
+    df_cat_pheno = pd.merge(df_var_t, df_cat_pivot, left_on='variable', right_on='variable').sort_values(by=['patientunitstayid','t','variable'])
 
-# In[161]:
+    df_final_x = pd.concat([df_t_pheno, df_cat_pheno]).sort_values(by=['patientunitstayid','t','variable'])
+    df_final_y = df[['patientunitstayid', 'survival_90days']].replace({'alive':0, 'death':1})
 
-
-df_reformat = pd.concat([v for k, v in df_msingles.items()], ignore_index=True)
-df_reformat = df_reformat.sort_values(by=['patientunitstayid','t', 'variable'])[['patientunitstayid','t','variable','value']]
-
-
-# In[162]:
-
-
-import numpy as np
-
-patient_to_id = {}
-patients = df['patientunitstayid'].drop_duplicates()
-i = 0
-for x in patients:
-    patient_to_id[x] = i
-    i += 1
-
-
-# In[163]:
-
-df_cat['id'] = df_cat['patientunitstayid'].apply(lambda x: patient_to_id[x])
-df_cat = df_cat.set_index('id')
-
-df_reformat['id'] = df_reformat['patientunitstayid'].apply(lambda x: patient_to_id[x])
-df_reformat = df_reformat.set_index('id')
-
-
-
-# In[164]:
-
-print('Saving to csvs')
-df_reformat.to_csv('eicu_prepared_x.csv')
-
-
-# In[165]:
-
-
-df_cat.to_csv('eicu_prepared_y.csv')
-
-
-# In[ ]:
-
-
-
-
+    df_final_x.to_csv('eicu_prepared_x.csv', index=False)
+    df_final_y.to_csv('eicu_prepared_y.csv', index=False)
