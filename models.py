c2b4fd5939936e002edcc02b58afd5c29fc91fef
meng2468 <mandm116@gmail.com>
2023-06-29 23:04:59 +0800

    Update dropout to between layers

--- models.py
+++ models.py
@@ -25,14 +25,14 @@ class BaseRecurrent(nn.Module):
     def __init__(self, input_size, h_size, dropout):
         super(BaseRecurrent, self).__init__()
         self.hidden_size = h_size
-        self.recurrent = nn.RNN(input_size=input_size, hidden_size=self.hidden_size, nonlinearity='relu')
+        self.recurrent = nn.RNN(input_size=input_size, hidden_size=self.hidden_size, dropout=dropout)
         self.final = nn.Linear(in_features=self.hidden_size,out_features=1)
         self.dropout = nn.Dropout(p=dropout)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         out, hidden = self.recurrent(x)
-        out = self.dropout(out)
+        # out = self.dropout(out)
         final = self.final(out[:,-1,:])
         logits = self.sigmoid(final)
         return logits
@@ -42,14 +42,14 @@ class BaseLSTM(nn.Module):
     def __init__(self, input_size, h_size, dropout):
         super(BaseLSTM, self).__init__()
         self.hidden_size = h_size
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=self.hidden_size)
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=self.hidden_size, dropout=dropout)
         self.final = nn.Linear(in_features=self.hidden_size, out_features=1)
         self.dropout = nn.Dropout(p=dropout)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         out, hidden = self.lstm(x)
-        out = self.dropout(out)
+        # out = self.dropout(out)
         final = self.final(out[:,-1,:])
         logits = self.sigmoid(final)
         return logits
@@ -58,14 +58,14 @@ class BaseGRU(nn.Module):
     def __init__(self, input_size, h_size, dropout):
         super(BaseGRU, self).__init__()
         self.hidden_size = h_size
-        self.lstm = nn.GRU(input_size=input_size, hidden_size=self.hidden_size)
+        self.lstm = nn.GRU(input_size=input_size, hidden_size=self.hidden_size, dropout=dropout)
         self.final = nn.Linear(in_features=self.hidden_size, out_features=1)
         self.dropout = nn.Dropout(p=dropout)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         out, hidden = self.lstm(x)
-        out = self.dropout(out)
+        # out = self.dropout(out)
         final = self.final(out[:,-1,:])
         logits = self.sigmoid(final)
         return logits
