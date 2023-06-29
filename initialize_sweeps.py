8d41aa4b30c26bdd7110fffa12524cc67f1caf22
meng2468 <mandm116@gmail.com>
2023-06-29 23:02:53 +0800

    Change to layer dropout

--- initialize_sweeps.py
+++ initialize_sweeps.py
@@ -4,42 +4,42 @@ import sys
 if __name__ == '__main__':
     sweep_configuration = {
         'method': 'random',
-        'name': 'mimic-sweep',
+        'name': 'mimic-rnn-relu',
         'entity': 'risk-prediction',
         'metric': {'goal': 'minimize', 'name': 'Final Val Loss'},
         'parameters': 
         {
             'learning_rate': {'max': 1e-3, 'min': 1e-6},
             'hidden_size': {'values': [512, 1024, 2048, 4096, 8192]},
-            'dropout': {'max': .5, 'min': 0.},
-            'model_name': {'values': ['gru','lstm','rnn']},
+            'dropout': {'values': [0, .1, .2, .3, .4, .5]},
+            'model_name': {'values': ['rnn', 'lstm','gru']},
             'batch_size': {'value': 50},
             'input_size': {'value': 51},
             'dataset': {'value':'mimic'}
         }
     }
-    project_name = 'mimic-random-sweep'
+    project_name = 'mimic-rnn-relu'
 
     sweep_id = wandb.sweep(sweep_configuration, project=project_name)
     print('MIMIC Sweep'+':', sweep_id, project_name)
 
     sweep_configuration = {
         'method': 'random',
-        'name': 'eicu-sweep',
+        'name': 'eicu-rnn-relu',
         'entity': 'risk-prediction',
         'metric': {'goal': 'minimize', 'name': 'Final Val Loss'},
         'parameters': 
         {
             'learning_rate': {'max': 1e-3, 'min': 1e-6},
             'hidden_size': {'values': [512, 1024, 2048, 4096, 8192]},
-            'dropout': {'max': .5, 'min': 0.},
-            'model_name': {'values': ['gru','lstm','rnn']},
+            'dropout': {'values': [0, .1, .2, .3, .4, .5]},
+            'model_name': {'values': ['rnn']},
             'batch_size': {'value': 50},
             'input_size': {'value': 45},
             'dataset': {'value':'eicu'}
         }
     }
-    project_name = 'eicu-random-sweep'
+    project_name = 'eicu-rnn-relu'
 
 
     sweep_id = wandb.sweep(sweep_configuration, project=project_name)
