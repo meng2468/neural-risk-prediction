6c4902786855038d59da0cfd61e1d72f9e5489a6
meng2468 <mandm116@gmail.com>
2023-07-04 20:16:17 +0800

    Remove dropout in model cells

--- models.py
+++ models.py
@@ -2,12 +2,12 @@ import os
 import torch
 from torch import nn
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cuda:1" if torch.cuda.is_available() else "cpu"
 # print(f"Using {device} device")
 
-class DiPole(nn.Module):
+class BaseRecurrent(nn.Module):
     def __init__(self, input_size, h_size, dropout):
-        super(DiPole, self).__init__()
+        super(BaseRecurrent, self).__init__()
         self.hidden_size = h_size
         self.recurrent = nn.RNN(input_size=input_size, hidden_size=self.hidden_size)
         self.final = nn.Linear(in_features=self.hidden_size,out_features=1)
@@ -21,35 +21,19 @@ class DiPole(nn.Module):
         logits = self.sigmoid(final)
         return logits
 
-class BaseRecurrent(nn.Module):
-    def __init__(self, input_size, h_size, dropout):
-        super(BaseRecurrent, self).__init__()
-        self.hidden_size = h_size
-        self.recurrent = nn.RNN(input_size=input_size, hidden_size=self.hidden_size, dropout=dropout)
-        self.final = nn.Linear(in_features=self.hidden_size,out_features=1)
-        self.dropout = nn.Dropout(p=dropout)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        out, hidden = self.recurrent(x)
-        # out = self.dropout(out)
-        final = self.final(out[:,-1,:])
-        logits = self.sigmoid(final)
-        return logits
-
 
 class BaseLSTM(nn.Module):
     def __init__(self, input_size, h_size, dropout):
         super(BaseLSTM, self).__init__()
         self.hidden_size = h_size
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=self.hidden_size, dropout=dropout)
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=self.hidden_size)
         self.final = nn.Linear(in_features=self.hidden_size, out_features=1)
         self.dropout = nn.Dropout(p=dropout)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         out, hidden = self.lstm(x)
-        # out = self.dropout(out)
+        out = self.dropout(out)
         final = self.final(out[:,-1,:])
         logits = self.sigmoid(final)
         return logits
@@ -58,14 +42,14 @@ class BaseGRU(nn.Module):
     def __init__(self, input_size, h_size, dropout):
         super(BaseGRU, self).__init__()
         self.hidden_size = h_size
-        self.lstm = nn.GRU(input_size=input_size, hidden_size=self.hidden_size, dropout=dropout)
+        self.lstm = nn.GRU(input_size=input_size, hidden_size=self.hidden_size)
         self.final = nn.Linear(in_features=self.hidden_size, out_features=1)
         self.dropout = nn.Dropout(p=dropout)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         out, hidden = self.lstm(x)
-        # out = self.dropout(out)
+        out = self.dropout(out)
         final = self.final(out[:,-1,:])
         logits = self.sigmoid(final)
         return logits
